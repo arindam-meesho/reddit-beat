947d0989f5bb5b439638e39558c4bf35fe915038
arindadm-meesho <arindampradhan10@gmail.com>
2019-08-31 09:57:32 +0530

    redux persist

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,15 @@ import { routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import createRootReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+  
 
 export const history = createHistory()
 
@@ -23,11 +32,16 @@ const composedEnhancers = compose(
 )
 
 export default function configureStore(preloadedState) {
+    const rootReducer = createRootReducer(history)
+    const persistedReducer = persistReducer(persistConfig, rootReducer)
+
     const store = createStore(
-        createRootReducer(history), // root reducer with router state
+        persistedReducer, // root reducer with router state
         preloadedState,
         composedEnhancers,
     )
+    let persistor = persistStore(store)
 
-    return store
+    
+    return {store,persistor}
 }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,20 @@ import { ConnectedRouter } from 'connected-react-router'
 import ScrollToTop from "./components/ScrollToTop";
 import { Provider } from 'react-redux'
 import configureStore, { history } from './configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const store = configureStore({})
+const {store , persistor}  = configureStore({})
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <ScrollToTop>
-                <App />
-            </ScrollToTop>
-        </ConnectedRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <ConnectedRouter history={history}>
+                <ScrollToTop>
+                    <App />
+                </ScrollToTop>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>
 , document.getElementById('root'));
 
